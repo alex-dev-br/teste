c04014e1411352fa63cbdbddba706c3c7537b47f
Kervin Candido <kervincandido@gmail.com>
2025-11-02 22:07:21 -0300

    feature: sinalizando campo opcionais
    adicionando nullable igual true para o swagger retornar que o campo ser nulo quando consultado

--- src/main/java/br/com/fiap/restaurantusersapi/infrastructure/adapters/inbound/rest/dto/AddressDTO.java
+++ src/main/java/br/com/fiap/restaurantusersapi/infrastructure/adapters/inbound/rest/dto/AddressDTO.java
@@ -9,13 +9,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Representação de endereço", name = "AddressResponse")
 public record AddressDTO (
-    @Schema(example = "Rua das Flores") String street,
-    @Schema(example = "123") String number,
-    @Schema(example = "Apto 101") String complement,
-    @Schema(example = "São Paulo") String city,
-    @Schema(example = "Pinheiros") String neighborhood,
-    @Schema(example = "SP", minLength = 2, maxLength = 2) String state,
-    @Schema(example = "12345-678", minLength = 8, maxLength = 9) String zipCode
+    @Schema(example = "Rua das Flores", nullable = true) String street,
+    @Schema(example = "123", nullable = true) String number,
+    @Schema(example = "Apto 101", nullable = true) String complement,
+    @Schema(example = "São Paulo", nullable = true) String city,
+    @Schema(example = "Pinheiros", nullable = true) String neighborhood,
+    @Schema(example = "SP", minLength = 2, maxLength = 2, nullable = true) String state,
+    @Schema(example = "12345-678", minLength = 8, maxLength = 9, nullable = true) String zipCode
 ) {
     public AddressDTO(AddressEntity address) {
         this(address.getStreet(), address.getNumber(), address.getComplement(), address.getCity(), address.getNeighborhood(), address.getState(), address.getZipCode());
